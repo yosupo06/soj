0e77de609dfe1d8821579dd63920551db6e735ea
yosupo <yosupo06@gmail.com>
2015-08-10 18:05:55 +0900

    コンパイルオプションを分離

--- soj.go
+++ soj.go
@@ -17,27 +17,6 @@ import (
 	"time"
 )
 
-var compileArg = map[string]string{
-	".cpp": "g++ -std=c++11 -O2 -Wl,-stack_size,0x10000000 {{.Name}}.cpp -o {{.Name}}",
-	".d":   "dmd -O {{.Name}}.d",
-	".cs":  "mcs -r:System.Numerics {{.Name}}.cs",
-}
-
-var testArg = map[string]string{
-	".cpp": "./{{.Name}} --seed={{.Seed}} --hash={{.Hash}}",
-	".d":   "./{{.Name}} --seed={{.Seed}} --hash={{.Hash}}",
-}
-
-var execArg = map[string]string{
-	".cpp": "./{{.Name}}",
-	".d":   "./{{.Name}}",
-	".cs":  "mono {{.Name}}.exe",
-}
-
-var checkerArg = map[string]string{
-	".cpp": "./{{.Name}} {{.Input}} {{.Output}} {{.Answer}}",
-}
-
 var tp = make(map[string]*template.Template)
 
 func init() {
