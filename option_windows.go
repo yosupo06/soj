34b38757933ec5f262a3652a17451ee02e92c599
yosupo <yosupo06@gmail.com>
2015-08-10 19:21:05 +0900

    hen kou

--- option_windows.go
+++ option_windows.go
@@ -3,20 +3,20 @@ package main
 var compileArg = map[string]string{
 	".cpp": "g++ -std=c++11 -O2 {{.Name}}.cpp -o {{.Name}}",
 	".d":   "dmd -m64 -O {{.Name}}.d",
-	".cs":  "mcs -r:System.Numerics {{.Name}}.cs",
+	".cs":  "mcs /r:System.Numerics {{.Name}}.cs",
 }
 
 var testArg = map[string]string{
-	".cpp": "./{{.Name}} --seed={{.Seed}} --hash={{.Hash}}",
-	".d":   "./{{.Name}} --seed={{.Seed}} --hash={{.Hash}}",
+	".cpp": "{{.Name}} --seed={{.Seed}} --hash={{.Hash}}",
+	".d":   "{{.Name}} --seed={{.Seed}} --hash={{.Hash}}",
 }
 
 var execArg = map[string]string{
-	".cpp": "./{{.Name}}",
-	".d":   "./{{.Name}}",
-	".cs":  "mono {{.Name}}.exe",
+	".cpp": "{{.Name}}.exe",
+	".d":   "{{.Name}}.exe",
+	".cs":  "{{.Name}}.exe",
 }
 
 var checkerArg = map[string]string{
-	".cpp": "./{{.Name}} {{.Input}} {{.Output}} {{.Answer}}",
+	".cpp": "{{.Name}}.exe {{.Input}} {{.Output}} {{.Answer}}",
 }
