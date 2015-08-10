9462aa797c7c726869939c9cd8948222c32c872f
yosupo <yosupo06@gmail.com>
2015-08-10 20:53:05 +0900

    fix

--- option_windows.go
+++ option_windows.go
@@ -3,7 +3,7 @@ package main
 var compileArg = map[string]string{
 	".cpp": "g++ -std=c++11 -O2 {{.Name}}.cpp -o {{.Name}}",
 	".d":   "dmd -m64 -O {{.Name}}.d",
-	".cs":  "csc /r:System.Numerics {{.Name}}.cs",
+	".cs":  "csc /r:System.Numerics.dll {{.Name}}.cs",
 }
 
 var testArg = map[string]string{
